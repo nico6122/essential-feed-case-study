db069ee79dce50bad6f0503a415d6c621c0eab0d
Nico <nico6122@gmail.com>
2021-07-23 11:52:26 -0400

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Nicolas Guasch on 23-07-21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
