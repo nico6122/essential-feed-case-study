add346887930abeef093cfe1bc28a067e6fff0ab
Nico <nico6122@gmail.com>
2021-08-16 07:29:40 -0400

    Delivers items array with 200 HTTP response with JSON items

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -40,9 +40,9 @@ public final class RemoteFeedLoader{
         client.get(from: url){ result in
             switch result{
             case let .success(data, _):
-                if let _ = try? JSONSerialization.jsonObject(with: data)
+                if let root = try? JSONDecoder().decode(Root.self, from:data)
                 {
-                    completion(.success([]))
+                    completion(.success(root.items))
                 }else{
                     completion(.failure(.invaildData))
                 }
@@ -52,6 +52,10 @@ public final class RemoteFeedLoader{
             }
         }
     }
+}
+
+private struct Root: Decodable{
+    let items: [FeedItem]
 }
 
 
